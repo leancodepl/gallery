95a8ab23f5579d3d4fefd7ddf96224bb3aa85112
Kamil Baranowski <kamil.baranowski@leancode.pl>
2023-05-26 18:41:57 +0200

    Setup IOS plfatorm for tests

---
+++ ios/RunnerUITests/RunnerUITests.m
@@ -0,0 +1,11 @@
+//
+//  RunnerUITests.m
+//  RunnerUITests
+//
+//  Created by Kamil Baranowski on 26/05/2023.
+//
+
+@import XCTest;
+@import patrol;
+
+PATROL_INTEGRATION_TEST_IOS_RUNNER(RunnerUITests)
